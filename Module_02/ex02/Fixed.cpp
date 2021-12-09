0ca897d2b11a191b027c4b38a99c60af3091cd2a
REDISKA88 <ANjune334>
2021-12-09 10:58:23 +0300

    complete

--- Module_02/ex02/Fixed.cpp
+++ Module_02/ex02/Fixed.cpp
@@ -10,12 +10,104 @@ int newVal = roundf(value * (1 << numOfBits));
 this->setRawBits(newVal);
 }
 
+Fixed &Fixed::min(Fixed &F1, Fixed &F2) {
+	if (F1 >= F2)
+		return (F2);
+	return (F1);
+}
+
+Fixed & Fixed::max(Fixed &F1, Fixed &F2) {
+	if (F1 <= F2)
+		return (F2);
+	return (F1);
+}
+
+const Fixed & Fixed::min(const Fixed &F1, const Fixed &F2) {
+	if ((Fixed&)F1 >= (Fixed&)F2)
+		return (F2);
+	return (F1);
+}
+
+const Fixed & Fixed::max(const Fixed &F1, const Fixed &F2) {
+	if ((Fixed&)F1 <= (Fixed&)F2)
+		return (F2);
+	return (F1);
+}
 float Fixed::toFloat() const {
 	return ((float)fixedPointValue / (1 << numOfBits));
 }
 
 int Fixed::toInt() const {
 	return (fixedPointValue >> numOfBits);
+}
+
+bool Fixed::operator!=(const Fixed &F) {
+	return (fixedPointValue != F.fixedPointValue);
+}
+
+bool Fixed::operator==(const Fixed &F) {
+	return (fixedPointValue == F.fixedPointValue);
+}
+
+bool Fixed::operator>(const Fixed &F) {
+	return(fixedPointValue > F.fixedPointValue);
+}
+
+bool Fixed::operator<(const Fixed &F) {
+	return (fixedPointValue < F.fixedPointValue);
+}
+
+bool Fixed::operator>=(const Fixed &F) {
+	return (fixedPointValue >= F.fixedPointValue);
+}
+bool Fixed::operator<=(const Fixed &F) {
+	return(fixedPointValue <= F.fixedPointValue);
+}
+Fixed Fixed::operator+(const Fixed &F2) {
+	Fixed Plus;
+	Plus.fixedPointValue = this->fixedPointValue + F2.getRawBits();
+	return (Plus);
+}
+Fixed Fixed::operator-(const Fixed &F2) {
+	Fixed Minus;
+	Minus.fixedPointValue = (this->fixedPointValue - F2.getRawBits());
+	return (Minus);
+}
+
+Fixed Fixed::operator/(const Fixed &F2) {
+	Fixed Divide;
+	Divide.fixedPointValue = ((this->fixedPointValue * (1 << Fixed::numOfBits)) / F2.getRawBits());
+	return (Divide);
+}
+
+Fixed Fixed::operator*(const Fixed &F2) {
+	Fixed Multiply;
+	Multiply.fixedPointValue = (this->fixedPointValue * (F2.getRawBits() / (1 << Fixed::numOfBits)));
+	return (Multiply);
+}
+
+Fixed Fixed::operator++(){
+	fixedPointValue++;
+	return (*this);
+}
+
+Fixed Fixed::operator--(){
+	--fixedPointValue;
+	return (*this);
+}
+
+Fixed Fixed::operator++(int){
+	Fixed F;
+	F.fixedPointValue = fixedPointValue;
+	++fixedPointValue;
+	return (F);
+}
+
+Fixed Fixed::operator--(int){
+	Fixed F;
+	F.fixedPointValue = fixedPointValue;
+	--fixedPointValue;
+	return (F);
 }
 
 std::ostream& operator<<(std::ostream &out, const Fixed &fixed) {
--- Module_02/ex02/Fixed.hpp
+++ Module_02/ex02/Fixed.hpp
@@ -13,21 +13,38 @@ private:
 
 public:
 
-	//new constructors:
 	Fixed(const int value);
 	Fixed(const float value);
-	//
 
 	Fixed();
 	Fixed(const Fixed&);
+
 	Fixed& operator=(const Fixed&);
+	bool operator!=(Fixed const &F);
+	bool operator==(Fixed const &F);
+	bool operator>(Fixed const &F);
+	bool operator<(Fixed const &F);
+	bool operator>=(Fixed const &F);
+	bool operator<=(Fixed const &F);
+	Fixed operator+(Fixed const &F2);
+	Fixed operator-(Fixed const &F2);
+	Fixed operator/(Fixed const &F2);
+	Fixed operator*(Fixed const &F2);
+
+	Fixed operator++();
+	Fixed operator--();
+	Fixed operator++(int);
+	Fixed operator--(int);
+
 	int getRawBits( void ) const;
 	void setRawBits( int const);
-
-	//new member functions:
 	float toFloat( void ) const;
 	int toInt( void ) const;
-	//
+
+	static Fixed &min(Fixed&, Fixed&);
+	static Fixed &max(Fixed&, Fixed&);
+	static const Fixed &min(const Fixed&, const Fixed&);
+	static const Fixed &max(const Fixed&, const Fixed&);
 
 	~Fixed();
 };
