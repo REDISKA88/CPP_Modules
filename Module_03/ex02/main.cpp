436237afadc92e2cb986538656399b46d12fc6d2
REDISKA88 <ANjune334>
2021-12-10 16:55:38 +0300

    Virtual inheritance (fixed main)

--- Module_03/ex02/main.cpp
+++ Module_03/ex02/main.cpp
@@ -1,10 +1,19 @@
-#include "ScavTrap.hpp"
-
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int    main()
 {
 
+	ClapTrap    clap("Zoey");
+	FragTrap    frag("Bill");
+
+	clap.attack("Ellis");
+	clap.takeDamage(10);
+	clap.beRepaired(10);
+	frag.attack("Ellis");
+	frag.takeDamage(10);
+	frag.beRepaired(10);
+	frag.highFivesGuys();
+	frag.highFivesGuys();
 
 	return (0);
 }
