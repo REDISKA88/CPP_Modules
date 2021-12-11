32ff740c2853eb62a0736f51e1738af6117bd6bf
REDISKA88 <>
2021-12-11 03:37:50 +0300

    add new class, overriding methods(fix)

--- Module_04/ex01/main.cpp
+++ Module_04/ex01/main.cpp
@@ -4,20 +4,7 @@
 
 int main(){
 
-	Animal* A = new Animal();
-	Animal* C = new Cat();
-	Animal* D = new Dog();
-	WrongAnimal* W = new WrongCat();
 
-	A->makeSound();
-	C->makeSound();
-	D->makeSound();
-	W->makeSound();
-
-	delete A;
-	delete C;
-	delete D;
-	delete W;
 
 	return 0;
 }
